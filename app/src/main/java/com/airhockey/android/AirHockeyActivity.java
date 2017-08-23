97e20abfeec58b723f798746c7c851e755d2b904
andychang <changzuozhen@qq.com>
2017-08-23 20:10:44 +0800

    AirHockey1

--- app/src/main/java/com/airhockey/android/AirHockeyActivity.java
+++ app/src/main/java/com/airhockey/android/AirHockeyActivity.java
@@ -6,7 +6,7 @@
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/kbogla for more book information.
 ***/
-package com.firstopenglproject.android;
+package com.airhockey.android;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -17,7 +17,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class FirstOpenGLProjectActivity extends Activity {
+public class AirHockeyActivity extends Activity {
     /**
      * Hold a reference to our GLSurfaceView
      */
@@ -27,18 +27,14 @@ public class FirstOpenGLProjectActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         glSurfaceView = new GLSurfaceView(this);
 
         // Check if the system supports OpenGL ES 2.0.
-        final ActivityManager activityManager =
+        ActivityManager activityManager = 
             (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
- 
-        final ConfigurationInfo configurationInfo =
-            activityManager.getDeviceConfigurationInfo();
-        /*
-		
-        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
-         */
+        ConfigurationInfo configurationInfo = activityManager
+            .getDeviceConfigurationInfo();
         // Even though the latest emulator supports OpenGL ES 2.0,
         // it has a bug where it doesn't set the reqGlEsVersion so
         // the above check doesn't work. The below will detect if the
@@ -46,27 +42,27 @@ public class FirstOpenGLProjectActivity extends Activity {
         // OpenGL ES 2.0.
         final boolean supportsEs2 =
             configurationInfo.reqGlEsVersion >= 0x20000
-                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 
                  && (Build.FINGERPRINT.startsWith("generic")
                   || Build.FINGERPRINT.startsWith("unknown")
-                  || Build.MODEL.contains("google_sdk")
+                  || Build.MODEL.contains("google_sdk") 
                   || Build.MODEL.contains("Emulator")
                   || Build.MODEL.contains("Android SDK built for x86")));
 
         if (supportsEs2) {
             // Request an OpenGL ES 2.0 compatible context.
-            glSurfaceView.setEGLContextClientVersion(2);
-
+            glSurfaceView.setEGLContextClientVersion(2);            
+            
             // Assign our renderer.
-            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+            glSurfaceView.setRenderer(new AirHockeyRenderer(this));
             rendererSet = true;
         } else {
             /*
              * This is where you could create an OpenGL ES 1.x compatible
-             * renderer if you wanted to support both ES 1 and ES 2. Since we're
-             * not doing anything, the app will crash if the device doesn't
-             * support OpenGL ES 2.0. If we publish on the market, we should
-             * also add the following to AndroidManifest.xml:
+             * renderer if you wanted to support both ES 1 and ES 2. Since 
+             * we're not doing anything, the app will crash if the device 
+             * doesn't support OpenGL ES 2.0. If we publish on the market, we 
+             * should also add the following to AndroidManifest.xml:
              * 
              * <uses-feature android:glEsVersion="0x00020000"
              * android:required="true" />
